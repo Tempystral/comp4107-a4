bfb11fad7c6f12b582fa0df18fe9bb3e172876f6
Tempystral <elijah.doern@gmail.com>
2018-11-30 02:21:00 -0500

    Updated dataset importer, cleaned up code

--- cnn.py
+++ cnn.py
@@ -1,14 +1,19 @@
 import tensorflow as tf
 import numpy as np
-from tensorflow.examples.tutorials.mnist import input_data
+import tensorflow.keras.utils as utils
+#from tensorflow.examples.tutorials.mnist import input_data
+from tensorflow.keras.datasets import cifar10
 tf.logging.set_verbosity(tf.logging.ERROR)
 
 batch_size = 128
 test_size = 256
 
+###########################
+######## Functions ########
+###########################
+
 def init_weights(shape):
     return tf.Variable(tf.random_normal(shape, stddev=0.01))
-
 
 def model(X, w, w_fc, w_o, p_keep_conv, p_keep_hidden):
     l1a = tf.nn.relu(tf.nn.conv2d(X, w,                       # l1a shape=(?, 28, 28, 32)
@@ -27,26 +32,38 @@ def model(X, w, w_fc, w_o, p_keep_conv, p_keep_hidden):
     pyx = tf.matmul(l4, w_o)
     return pyx
 
-mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
-trX, trY, teX, teY = mnist.train.images, mnist.train.labels, mnist.test.images, mnist.test.labels
-trX = trX.reshape(-1, 28, 28, 1)  # 28x28x1 input img
-teX = teX.reshape(-1, 28, 28, 1)  # 28x28x1 input img
+############################
+######## Parameters ########
+############################
 
-X = tf.placeholder("float", [None, 28, 28, 1])
-Y = tf.placeholder("float", [None, 10])
+with tf.name_scope("Data") as scope:                    # Training data shape: (50000, 32, 32, 3)
+    (x_train, Ytr), (x_test, Yte) = cifar10.load_data() # Testing data shape: (10000, 32, 32, 3)
+    y_train = utils.to_categorical(Ytr)                 # Training label shape: (50000, 10)
+    y_test = utils.to_categorical(Yte)                  # Testing label shape: (10000, 10)
 
-w = init_weights([3, 3, 1, 32])       # 3x3x1 conv, 32 outputs
-w_fc = init_weights([32 * 14 * 14, 625]) # FC 32 * 14 * 14 inputs, 625 outputs
-w_o = init_weights([625, 10])         # FC 625 inputs, 10 outputs (labels)
+with tf.name_scope("Placeholders") as scope:
+    X = tf.placeholder("float", [None, 28, 28, 1])
+    Y = tf.placeholder("float", [None, 10])
 
-p_keep_conv = tf.placeholder("float")
-p_keep_hidden = tf.placeholder("float")
-py_x = model(X, w, w_fc, w_o, p_keep_conv, p_keep_hidden)
+with tf.name_scope("Weights") as scope:
+    w = init_weights([3, 3, 1, 32])       # 3x3x1 conv, 32 outputs
+    w_fc = init_weights([32 * 14 * 14, 625]) # FC 32 * 14 * 14 inputs, 625 outputs
+    w_o = init_weights([625, 10])         # FC 625 inputs, 10 outputs (labels)
 
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=py_x, labels=Y))
-train_op = tf.train.RMSPropOptimizer(0.001, 0.9).minimize(cost)
-predict_op = tf.argmax(py_x, 1)
+with tf.name_scope("Model") as scope:
+    p_keep_conv = tf.placeholder("float")
+    p_keep_hidden = tf.placeholder("float")
+    py_x = model(X, w, w_fc, w_o, p_keep_conv, p_keep_hidden)
 
+with tf.name_scope("Functions") as scope:
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=py_x, labels=Y))
+    train_op = tf.train.RMSPropOptimizer(0.001, 0.9).minimize(cost)
+    predict_op = tf.argmax(py_x, 1)
+
+#########################
+######## Runtime ########
+#########################
+'''
 # Launch the graph in a session
 with tf.Session() as sess:
     # you need to initialize all variables
@@ -66,4 +83,4 @@ with tf.Session() as sess:
         print(i, np.mean(np.argmax(teY[test_indices], axis=1) ==
                          sess.run(predict_op, feed_dict={X: teX[test_indices],
                                                          p_keep_conv: 1.0,
-                                                         p_keep_hidden: 1.0})))
+                                                         p_keep_hidden: 1.0})))'''
